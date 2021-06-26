b6c53b08cccb0e051ac4dca699dc3e3c71cfb189
Edgar Gonzalez <edgar.gonzalez@utexas.edu>
2021-06-26 02:42:05 -0500

    Functional Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,24 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
 
 
-class CampsiteInfo extends Component{ 
-        constructor(props) {
-            super(props);
-            this.state = {
-            selectedCampsite: null};
-        }
-
-        onCampsiteSelect(campsite) {
-            this.setState({selectedCampsite: campsite});
-        }
-
-
-
-renderCampsite(campsite) {
+function RenderCampsite({campsite}) {
     if (campsite) {
         return (
-         <div className='col-md-5 m-1'>
+        <div className='col-md-5 m-1'>
             <Card>
                 <CardImg top src={campsite.image} alt={campsite.name} />
                 <CardBody>
@@ -27,14 +14,14 @@ renderCampsite(campsite) {
                 </CardBody>
             </Card>
         </div>
-         );
+        );
 
     }
     return <div />;
 }
 
 
-renderComments(comments) {
+function RenderComments({comments}) {
     if (comments) {
         return (
             <div className='col-md-5 m-1'>
@@ -57,17 +44,18 @@ renderComments(comments) {
 
 
 
-  render() {
-    if (this.props.campsite) {
-            return (
-                <div className='row'>
-                {this.renderCampsite(this.props.campsite)}
-                {this.renderComments(this.props.campsite.comments)}
+function CampsiteInfo(props) {
+    if (props.campsite) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments comments={props.campsite.comments} />
                 </div>
-                    )
-            }   
-                return (<div />) 
-                    
+            </div>
+        );
     }
+    return <div />;
 }
+
 export default CampsiteInfo;
